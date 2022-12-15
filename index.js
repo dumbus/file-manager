e1df1b212f1c8450c08a2e99a424e6a62a8d8461
dumbus <dumbusx@gmail.com>
2022-12-15 17:57:18 +0300

    feat: add directoryStorage module

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import { homedir } from 'node:os';
+
 import { printStartMessage, printEndMessage } from './utils/greeter.js';
+import { directoryStorage } from './utils/directoryStorage.js';
+
+const INVALID_INPUT_MESSAGE = 'Invalid input';
+const OPERATION_FAILED_MESSAGE = 'Operation failed';
+const CURRENT_DIRECTORY_MESSAGE = 'You are currently in ';
+
+directoryStorage.setCurrentDirectory(homedir());
 
 await printStartMessage();
--- utils/pathGenerator.js
+++
@@ -1,8 +0,0 @@
-import { homedir } from 'node:os';
-import { join } from 'node:path';
-
-export let homedir = homedir();
-
-export const getLastDirectory = (...args) => {
-    return join(...args);
-}
