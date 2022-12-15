bfba0786bdf4dce9c3496afaca5d78faff0f81c9
dumbus <dumbusx@gmail.com>
2022-12-15 18:17:34 +0300

    feat: add readline interface

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 import { homedir } from 'node:os';
+import { stdin as input, stdout as output } from 'node:process';
+import readline from 'node:readline/promises';
 
 import { printStartMessage, printEndMessage } from './utils/greeter.js';
 import { directoryStorage } from './utils/directoryStorage.js';
+import { readlink } from 'node:fs';
 
 const INVALID_INPUT_MESSAGE = 'Invalid input';
 const OPERATION_FAILED_MESSAGE = 'Operation failed';
 const CURRENT_DIRECTORY_MESSAGE = 'You are currently in ';
 
+await printStartMessage();
+
 directoryStorage.setCurrentDirectory(homedir());
 
-await printStartMessage();
+console.log(CURRENT_DIRECTORY_MESSAGE + directoryStorage.getCurrentDirectory());
+
+const rl = readline.createInterface({ input, output });
+
+rl.prompt();
+
+rl.on('line', async (input) => {
+    const trimmedInput = input.trim();
+
+    if (trimmedInput === '.exit') {
+        rl.close();
+    } else {
+        // handle user commands
+    }
+});
+
+rl.on('SIGINT', () => {
+    rl.close();
+    console.log('\r');
+});
+
+rl.on('close', async () => {
+    await printEndMessage();
+});
